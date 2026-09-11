210c9016c71d174fb26590d6dec67880eda2b25d
nikitaagg1993 <nikitaagg1993@users.noreply.github.com>
2026-09-11 18:33:32 +0530

    Add message send functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
 import styles from "./App.module.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  function handleContentSend(content) {
+    setMessages((prevMessages) => [...prevMessages, { content, role: "user" }]);
+  }
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <img className={styles.Logo} src="/chat-bot.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer} />
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls onSend={handleContentSend} />
     </div>
   );
 }
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,18 @@
+import styles from "./Chat.module.css";
+
+const WELCOME_MESSAGE = {
+  role: "assistant",
+  content: "Hello! How can I assist you right now?",
+};
+
+export function Chat({ messages }) {
+  return (
+    <div className={styles.Chat}>
+      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
+        <div key={index} className={styles.Message} data-role={role}>
+          {content}
+        </div>
+      ))}
+    </div>
+  );
+}
