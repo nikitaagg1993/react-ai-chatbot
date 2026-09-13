856fa3e8075fa44e67b37031575a8d003fed278c
nikitaagg1993 <nikitaagg1993@users.noreply.github.com>
2026-09-13 19:42:38 +0530

    Support Individual Chat History for Google AI

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -5,83 +5,87 @@ import { Controls } from "../Controls/Controls";
 import styles from "./Chat.module.css";
 
 export function Chat({
-  assistant,
-  chatId,
-  chatMessages,
-  onChatMessagesUpdate,
+    assistant,
+    chatId,
+    chatMessages,
+    onChatMessagesUpdate,
 }) {
-  const [messages, setMessages] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isStreaming, setIsStreaming] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isStreaming, setIsStreaming] = useState(false);
 
-  useEffect(() => {
-    setMessages(chatMessages);
-  }, [chatId]);
+    useEffect(() => {
+        setMessages(chatMessages);
 
-  useEffect(() => {
-    onChatMessagesUpdate(messages);
-  }, [messages]);
-
-  function updateLastMessageContent(content) {
-    setMessages((prevMessages) =>
-      prevMessages.map((message, index) =>
-        index === prevMessages.length - 1
-          ? { ...message, content: `${message.content}${content}` }
-          : message
-      )
-    );
-  }
-
-  function addMessage(message) {
-    setMessages((prevMessages) => [...prevMessages, message]);
-  }
-
-  async function handleContentSend(content) {
-    addMessage({ content, role: "user" });
-    setIsLoading(true);
-    try {
-      const result = await assistant.chatStream(
-        content,
-        messages.filter(({ role }) => role !== "system")
-      );
-
-      let isFirstChunk = false;
-      for await (const chunk of result) {
-        if (!isFirstChunk) {
-          isFirstChunk = true;
-          addMessage({ content: "", role: "assistant" });
-          setIsLoading(false);
-          setIsStreaming(true);
+        if (assistant?.name === "googleai") {
+            assistant.createChat(chatMessages);
         }
+    }, [chatId]);
 
-        updateLastMessageContent(chunk);
-      }
+    useEffect(() => {
+        onChatMessagesUpdate(messages);
+    }, [messages]);
 
-      setIsStreaming(false);
-    } catch (error) {
-      addMessage({
-        content:
-          error?.message ??
-          "Sorry, I couldn't process your request. Please try again!",
-        role: "system",
-      });
-      setIsLoading(false);
-      setIsStreaming(false);
+    function updateLastMessageContent(content) {
+        setMessages((prevMessages) =>
+            prevMessages.map((message, index) =>
+                index === prevMessages.length - 1
+                    ? { ...message, content: `${message.content}${content}` }
+                    : message
+            )
+        );
     }
-  }
 
-  return (
-    <>
-      {isLoading && <Loader />}
+    function addMessage(message) {
+        setMessages((prevMessages) => [...prevMessages, message]);
+    }
 
-      <div className={styles.Chat}>
-        <Messages messages={messages} />
-      </div>
+    async function handleContentSend(content) {
+        addMessage({ content, role: "user" });
+        setIsLoading(true);
+        try {
+            const result = await assistant.chatStream(
+                content,
+                messages.filter(({ role }) => role !== "system")
+            );
 
-      <Controls
-        isDisabled={isLoading || isStreaming}
-        onSend={handleContentSend}
-      />
-    </>
-  );
+            let isFirstChunk = false;
+            for await (const chunk of result) {
+                if (!isFirstChunk) {
+                    isFirstChunk = true;
+                    addMessage({ content: "", role: "assistant" });
+                    setIsLoading(false);
+                    setIsStreaming(true);
+                }
+
+                updateLastMessageContent(chunk);
+            }
+
+            setIsStreaming(false);
+        } catch (error) {
+            addMessage({
+                content:
+                    error?.message ??
+                    "Sorry, I couldn't process your request. Please try again!",
+                role: "system",
+            });
+            setIsLoading(false);
+            setIsStreaming(false);
+        }
+    }
+
+    return (
+        <>
+            {isLoading && <Loader />}
+
+            <div className={styles.Chat}>
+                <Messages messages={messages} />
+            </div>
+
+            <Controls
+                isDisabled={isLoading || isStreaming}
+                onSend={handleContentSend}
+            />
+        </>
+    );
 }
